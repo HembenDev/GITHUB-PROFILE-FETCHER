63f884805db46e9f386ebc6b0e6f8bb9d615da3a
Hemben <jeremiahhemben@gmail.com>
2025-05-30 18:34:59 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 async function fetchProfile(){
-  const
+  const userName = document.getElementById('username').value.trim();
+  const profileDiv = document.getElementById('profile');
+  const reposDiv = document.getElementById('repos');
+  const errorDiv = document.getElementById('error');
+
+  profileDiv.innerHTML = '';
+  reposDiv.innerHTML = '';
+  errorDiv.innerHTML = '';
+
+  if(!userName){
+    errorDiv.textContent = 'Please enter a username!';
+    return;
+  }
+   
+  try{
+    const[userResponse, reposResponse] = await Promise.all([
+      fetch(`https://api.github.com/users/${userName}`),
+      fetch(`https://api.github.com/users/${userName}/repos?per_page=5`)
+    ]);
+    if(!userResponse.ok) throw new Error('User not found!');
+    if(!reposResponse.ok) throw new Error('Error fetching repositories!')
+  }
 }
